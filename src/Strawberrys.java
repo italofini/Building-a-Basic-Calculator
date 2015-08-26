9185d560abc1367cf900cf893ac1c7ba90cdf39f
italofini <italo.fini@techhigh.us>
2015-08-25 23:36:49 -0400

    Bulding a Basic Calculator

---
+++ src/Strawberrys.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class Strawberrys {
+	public static void main(String args[]){
+		Scanner bucky = new Scanner (System.in);
+		double fnum, snum, answer;
+		System.out.println("Enter first num: ");
+		fnum = bucky.nextDouble();
+		System.out.println("Enter Second num: ");
+		snum = bucky.nextDouble();
+		answer = fnum + snum;
+		System.out.println(answer);
+		
+	}
+
+}
